a7d44d5e14f80a9119a905078075a6abd8159095
saralimbu2017 <iamsebi7@gmail.com>
2021-12-24 13:26:35 +1100

    Updating src of img with accessor set in Post model

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,7 +5,7 @@
     @foreach($posts as $post)
       <div class="image-container">
         <p>hello</p>
-        <img height="100" src="/images/{{$post->path}}" alt="">
+        <img height="100" src="{{$post->path}}" alt="">
       </div>
       <li><a href="{{route('posts.show',$post->id)}}">{{$post->title}}</a></li>
     @endforeach
