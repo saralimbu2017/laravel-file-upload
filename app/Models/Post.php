35f7ff265539b6132605e47064ad6a7363afea57
saralimbu2017 <iamsebi7@gmail.com>
2021-12-24 13:25:44 +1100

    Setting accessor property

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,9 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    public $directory = "/images/";
     protected $fillable = [
         'title',
         'content',
         'path'
-    ];
+    ]; 
+
+    public function getPathAttribute($value){
+        return $this->directory.$value;
+    }
 }
